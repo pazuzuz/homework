0f9b8e2bb1ee9ee1f61146e5e0dfde0fb2d177f3
pazuzu <zlukab83@gmail.com>
2019-05-19 21:02:05 +0300

    finished 3nd task.

---
+++ sandbox/src/test/java/stqa/pft/sandbox/point_tests/PointDistanceTest.java
@@ -0,0 +1,24 @@
+package stqa.pft.sandbox.point_tests;
+
+import figures.Point;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utils.UtilCalculations;
+
+public class PointDistanceTest {
+    @DataProvider
+    public Object[][] testingData(){
+        return new Object[][]{
+                            {new Point(0, 0), new Point(3, 4), 5.0},
+                            {new Point(3, 4), new Point(6,8), 5.0},
+                            {new Point(6, 8), new Point(0,0), 10.0},
+                            {new Point(-6, -8), new Point(0,0), 10.0},
+        };
+    }
+
+    @Test(description = "tests for points distance.", dataProvider = "testingData")
+    public void checkDistanceBetweenToPoints(Point p1, Point p2, double distance){
+        Assert.assertEquals(UtilCalculations.distance(p1, p2), distance, "Distance is incorrect.");
+    }
+}
