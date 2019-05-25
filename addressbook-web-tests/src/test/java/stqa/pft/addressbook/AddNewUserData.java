c383cbe267292816af5ddf12e6a0de06ece856ae
pazuzu <zlukab83@gmail.com>
2019-05-26 00:53:07 +0300

    finished 4th homework.

---
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/AddNewUserData.java
@@ -0,0 +1,37 @@
+package stqa.pft.addressbook;
+
+public class AddNewUserData {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String mobile;
+    private final String email;
+
+    public AddNewUserData(String firstName, String lastName, String address, String mobile, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getMobile() {
+        return this.mobile;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/GroupCreationTest.java
@@ -20,33 +20,61 @@ public class GroupCreationTest {
     WebDriverManager.chromedriver().setup();
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='LOGIN']")).click();
-  }
-
-  @Test
-  public void testGroupCreation(){
-    wd.findElement(By.linkText("GROUPS")).click();
-    wd.findElement(By.name("new")).click();
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test1");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test2");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test3");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
+    login("admin", "secret");
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown(){
-    wd.findElement(By.linkText("LOGOUT")).click();
+    logout();
     wd.quit();
+  }
+
+  @Test
+  public void testGroupCreation(){
+    goToGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='LOGIN']")).click();
+  }
+
+  private void goToGroupPage() {
+    wd.findElement(By.linkText("GROUPS")).click();
+  }
+
+  private void initGroupCreation(){
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void submitGroupCreation(){
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("LOGOUT")).click();
   }
 
   private boolean isElementPresent(By by) {
