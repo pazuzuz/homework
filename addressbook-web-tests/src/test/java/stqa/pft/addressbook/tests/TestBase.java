4bf18110aa4231a024191fb82c24e8975a77c2e7
ysteshenko <ext-yevhen.steshenko@here.com>
2019-06-27 14:11:10 +0300

    add logger

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/TestBase.java
@@ -1,24 +1,40 @@
 package stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import stqa.pft.addressbook.application_manager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    private Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp() throws IOException {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] o){
+        logger.info("Start test  " + method.getName() + " with parameters " + Arrays.asList(o));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method method){
+        logger.info("Stop test  " + method.getName());
+    }
 }
